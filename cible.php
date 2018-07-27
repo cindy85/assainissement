9ac9c58e0441c584b51e9af7e05db25313184efc
cindy85 <33489668+cindy85@users.noreply.github.com>
2018-07-27 14:16:12 +0200

    ajout fichier php pour formuliare

---
+++ cible.php
@@ -0,0 +1,21 @@
+<?php
+header('content-type: text/html; charset=utf-8');
+if(isset($_POST["btn"])){
+    $nom= $_POST['nom'];
+$email= $_POST['email'];
+$telephone= $_POST['telephone'];
+$message= $_POST['message'];
+$destinataire="cindyb85@hotmail.fr,bogaert.c@wanadoo.fr";
+$sujet="contact site web";
+$entete='From: '.$email.'';
+$message ='nom: '.$nom.''."\n".
+            'telephone: '.$telephone.''."\n".
+    
+    'contenumessage: '.$message.'';
+mail($destinataire, $sujet, $message, $entete);
+header('location:https://www.locationvacancesvendee.fr/message-envoi.html');
+}
+else{
+echo 'erreur de message'; 
+}
+?>
